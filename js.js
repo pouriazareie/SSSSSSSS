1cbdd0f91dbc703cf9e23c208e4600135e883f68
pouriazareie <pouria.zareie@gmail.com>
2019-11-02 21:30:54 +0330

    Create js.js

---
+++ js.js
@@ -0,0 +1,65 @@
+for (var i=0; i<10; i++) (function(t) {
+  window.setTimeout(function() {
+function stringGen(len) {
+  var text = "";
+  
+  var charset = "GHIJKLMNOPQRSTUVWXYZefghijklmnopqrstuvwxyz0123456789";
+  
+  for (var i = 0; i < len; i++)
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+   
+  return text;
+
+}
+document.getElementById("topcmm-123flashchat-mainview-top-settings-btn").click();
+
+document.querySelector("#topcmm-123flashchat-settings-menuitem-signin").click();
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+sleep(1000).then(() => {
+
+
+
+
+
+
+
+
+document.querySelector("#topcmm-123flashchat-sign-in-guest-checkbox-block").style="display: visable;";
+
+document.querySelector("#topcmm-123flashchat-loginview-username-input").value=stringGen(9);
+
+
+
+document.querySelector("#topcmm-123flashchat-loginview-guest-checkbox").click();
+
+document.querySelector("#topcmm-123flashchat-loginview-login-btn > span > span").click();
+
+});
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+sleep(1000).then(() => {
+
+document.querySelector("#topcmm-123flashchat-main-message-input").value='goh mizanam to chat room alireza                joooooooooooon:D';
+
+
+
+document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
+
+});
+
+
+
+
+    
+ }, t*2000)
+}(i))
+
+
+   
+   
